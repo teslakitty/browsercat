53b61b58678f98c1983fa6091139fa7e5c686dd8
James <jameskuyt655@gmail.com>
2024-11-24 09:11:46 -0500

    removed mainfragment from MainActivity.kt file

--- app/src/main/java/com/teslakitty/browsercat/MainActivity.kt
+++ app/src/main/java/com/teslakitty/browsercat/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 
 /**
- * Loads [MainFragment].
+ * MainActivity for BrowserCat app.
  */
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_browse_fragment, MainFragment())
-                .commitNow()
-        }
     }
 }
